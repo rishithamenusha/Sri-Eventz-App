9fcd8c1d6a672176a83ad0d9ed956d5391bba7f6
rishithamenusha <rishithamenusha@hotmail.com>
2021-09-21 10:57:36 +0530

    first commit

---
+++ lib/pallete.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const TextStyle kobodyText = TextStyle(fontSize: 22, color: Colors.white, height:1.5);
+
+const Color kWhite = Colors.white;
+const Color kblue = Color(0xff5663ff);
---
+++ lib/screens/forgot-password.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:frontend/pallete.dart';
+import 'package:frontend/widgets/background-image.dart';
+import 'package:frontend/widgets/rounded-button.dart';
+import 'package:frontend/widgets/text-field-input.dart';
+
+class ForgotPassword extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Stack(
+      children: [
+        BackgroundImage(image: 'assets/images/regis.png'),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Icon(
+                Icons.arrow_back_ios,
+                color: kWhite,
+              ),
+            ),
+            title: Text(
+              'Forgot Password',
+              style: kobodyText,
+            ),
+            centerTitle: true,
+          ),
+          body: Column(
+            children: [
+              Center(
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: size.height * 0.1,
+                    ),
+                    Container(
+                      width: size.width * 0.8,
+                      child: Text(
+                        'Enter your email we will send instruction to reset your password',
+                        style: kobodyText,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextInputField(
+                      icon: FontAwesomeIcons.envelope,
+                      hint: 'Email',
+                      Inputtype: TextInputType.emailAddress,
+                      inputAction: TextInputAction.done,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    RoundedButton(buttonName: 'Send')
+                  ],
+                ),
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
